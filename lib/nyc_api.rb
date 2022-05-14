7bf0048209c13ffc85e4dfe268dc16e9a8ce2fb4
George-Okumu <gokumu12@gmail.com>
2022-05-14 10:12:54 +0300

    worked with external Api

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -1,7 +1,8 @@
 require 'net/http'
 require 'open-uri'
 require 'json'
- 
+require "awesome_print" 
+
 class GetPrograms
 
   URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
@@ -9,10 +10,20 @@ class GetPrograms
   def get_programs
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
-    response.body
+    JSON.parse(response.body)
   end
 
+  def program_school
+    # we use the JSON library to parse the API response into nicely formatted JSON
+      programs = self.get_programs
+      programs.collect do |program|
+        program["agency"]
+      end
+    end
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+# programs = GetPrograms.new.get_programs
+# ap programs
+
+programs = GetPrograms.new
+puts programs.program_school.uniq
